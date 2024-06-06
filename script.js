d423d508f721fd17c6cdb2f4c644392574122d88
Itan87X <166138396+Itan87X@users.noreply.github.com>
2024-06-06 14:17:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -206,3 +206,66 @@ document.addEventListener('DOMContentLoaded', function() {
         return true;
     }
 });
+
+
+
+    // Validación de formulario de inicio de sesión
+    const loginForm = document.getElementById('loginForm');
+    if (loginForm) {
+        loginForm.addEventListener('submit', function(event) {
+            event.preventDefault(); // Evita que el formulario se envíe
+
+            let username = document.getElementById('username').value.trim();
+            let password = document.getElementById('password').value.trim();
+            let errorMessage = document.getElementById('error-message');
+
+            if (username === '' || password === '') {
+                errorMessage.style.display = 'block';
+            } else {
+                errorMessage.style.display = 'none';
+                $('#successModal').modal('show'); // Mostrar el modal de Bootstrap
+            }
+        });
+    }
+
+    // Redirigir al hacer clic en el botón "Aceptar" del modal
+    const redirectBtn = document.getElementById('redirectBtn');
+    if (redirectBtn) {
+        redirectBtn.addEventListener('click', function() {
+            window.location.href = 'divisas.html'; // URL de la página que consume la API de divisas
+        });
+    }
+    
+    // Validación de datos del formulario
+    function validarFormulario() {
+        var nameInput = document.getElementById('name');
+        var emailInput = document.getElementById('email');
+        var phoneInput = document.getElementById('phone');
+
+        var name = nameInput.value.trim();
+        var email = emailInput.value.trim();
+        var phone = phoneInput.value.trim();
+
+        // Validar el campo de nombre
+        if (!/^[a-zA-Z]+$/.test(name)) {
+            alert('Por favor, ingresa un nombre válido (solo letras).');
+            return false;
+        }
+
+        // Validar el campo de correo electrónico
+        var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) {
+            alert('Por favor, ingresa un correo electrónico válido.');
+            return false;
+        }
+
+        // Validar el campo de número de teléfono celular
+        var phoneRegex = /^\d{10}$/;
+        if (!phoneRegex.test(phone)) {
+            alert('Por favor, ingresa un número de teléfono celular válido (10 dígitos).');
+            return false;
+        }
+
+        return true;
+    }
+});
